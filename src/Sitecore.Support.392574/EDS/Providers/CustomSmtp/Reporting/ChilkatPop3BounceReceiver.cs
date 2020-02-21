ffbcce9e9d14103d92890a06cd33a6264a8b70f5
akrupskiy <anr@sitecore.net>
2020-02-21 13:44:10 +0200

    fix issue

--- src/Sitecore.Support.392574/EDS/Providers/CustomSmtp/Reporting/ChilkatPop3BounceReceiver.cs
+++ src/Sitecore.Support.392574/EDS/Providers/CustomSmtp/Reporting/ChilkatPop3BounceReceiver.cs
@@ -1,5 +1,6 @@
 ﻿namespace Sitecore.Support.EDS.Providers.CustomSmtp.Reporting
 {
+    using System;
     using Sitecore.EDS.Core.Diagnostics;
     using Sitecore.EDS.Core.Net.Pop3;
     using Sitecore.EDS.Core.Reporting;
@@ -24,7 +25,17 @@
 
         public override BounceStatus InspectEmail(IPop3Мail pop3Мail, IPop3Client pop3Client)
         {
-            return base.InspectEmail(pop3Мail, pop3Client);
+            try
+            {
+                return base.InspectEmail(pop3Мail, pop3Client);
+            }
+            catch (Exception ex)
+            {
+                var messageSubject = pop3Мail.GetHeader("Subject") != null ? pop3Мail.GetHeader("Subject") : "[no subject]";
+                var messageDate = pop3Мail.GetHeader("Date") != null ? pop3Мail.GetHeader("Date") : "[no date]";
+                this.logger.TraceWarn("Bounce status of \"" + messageSubject + "\" message cannot be resolved due to " + ex.GetType().ToString() + ", skipping. Datetime: " + messageDate + ". UIDL: " + pop3Мail.Uidl);
+                return BounceStatus.NotBounce;
+            }
         }
     }
 }
